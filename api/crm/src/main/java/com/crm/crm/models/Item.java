7aebdb541ca949d80c7a9f7c9cbc4ae399ba901a
Johannes <joecthomsen@gmail.com>
2022-05-21 21:25:49 +0200

    Model classes added

---
+++ api/crm/src/main/java/com/crm/crm/models/Item.java
@@ -0,0 +1,23 @@
+package com.crm.crm.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Item {
+
+    private int itemId;
+    private String itemName;
+    private int itemQuantity;
+    private float itemPrice;
+    private int belongsToOrder;
+
+    
+}
